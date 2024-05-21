1ee6e20f3b8bc677da8445bce9d2b9c576315190
Anida Nezovic <anezovic1@etf.unsa.ba>
2024-05-21 14:36:41 +0200

    layout for create article done

--- src/app/modules/articles/articles.routes.ts
+++ src/app/modules/articles/articles.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ArticlesComponent } from './components/articles/articles.component';
 import { ArticleDetailComponent } from './components/article-detail/article-detail.component';
+import { CreateArticleComponent } from './components/create-article/create-article.component';
 
 export default [
   {
@@ -11,5 +12,9 @@ export default [
   {
     path: 'article/:id',
     component: ArticleDetailComponent,
+  },
+  {
+    path: 'create-article',
+    component: CreateArticleComponent,
   },
 ] as Routes;
--- src/app/modules/articles/components/create-article/create-article.component.ts
+++ src/app/modules/articles/components/create-article/create-article.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-article',
   standalone: true,
   imports: [],
   templateUrl: './create-article.component.html',
-  styleUrl: './create-article.component.scss'
+  styleUrl: './create-article.component.scss',
 })
 export class CreateArticleComponent {
+  constructor(private router: Router) {}
 
+  openAllArticles() {
+    this.router.navigate(['/articles']);
+  }
 }
